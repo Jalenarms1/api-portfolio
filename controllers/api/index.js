637011a5a792601b81474e8a67f285dba771f312
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-10-12 05:46:28 -0700

    tweaking live config

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,7 +5,7 @@ const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
+    api_key: "132219615124213",
     api_secret: process.env.API_SECRET,
     secure: true
 })
